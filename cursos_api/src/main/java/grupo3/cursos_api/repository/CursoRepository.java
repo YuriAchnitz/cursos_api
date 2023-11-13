83f6394d6fe989627d8b2e7fa3629d2639751221
Yuri Achnitz <71293012+YuriAchnitz@users.noreply.github.com>
2023-11-13 03:29:38 -0300

    Delete cursos_api directory

--- cursos_api/src/main/java/grupo3/cursos_api/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package grupo3.cursos_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import grupo3.cursos_api.modelo.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Long>{
-
-}
--- cursos_api/src/main/java/grupo3/cursos_api/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package grupo3.cursos_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import grupo3.cursos_api.modelo.Professor;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long>{
-
-}
